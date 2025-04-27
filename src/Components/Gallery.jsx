ff24d19488fa4f47d4acbc582837365ee6c9684b
Mauricio <Mauriciogamboa00@gmail.com>
2025-04-27 13:33:50 -0400

    Implement Gallery component that displays list of tours

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,24 @@
+// Task 3: Map Tours in a Gallery Component
+
+import React from "react";
+import TourCard from "./TourCard";
+
+const Gallery = ({ tours, onRemoveTour }) => {
+  return (
+    <div className="gallery">
+      {tours.map((tour) => (
+        <TourCard
+          key={tour.id}
+          id={tour.id}
+          name={tour.name}
+          info={tour.info}
+          image={tour.image}
+          price={tour.price}
+          onRemove={onRemoveTour}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
